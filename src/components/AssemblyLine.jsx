d131823d985fa3a0fe2c00374c50c95da45685b1
Nazim <nazim.messaoudene@pm.me>
2024-03-06 03:02:15 +0100

    comments on code

--- src/components/AssemblyLine.jsx
+++ src/components/AssemblyLine.jsx
@@ -15,8 +15,8 @@ const AssemblyLine = props => {
         <button
               data-testid="assembly-item"
               key = {i}
-              onClick = {() => moveButtonToLeft (stage, input, i)}
-              onContextMenu={(e)=> moveButtonToRight (e, stage, input, i)}
+              onClick = {() => moveButtonToLeft (stage, input, i)} // clic avec bouton gauche de la souris
+              onContextMenu={(e)=> moveButtonToRight (e, stage, input, i)} // clic avec bouton droit de la souris (contextuel)
            >
               {input}
         </button>
@@ -25,7 +25,7 @@ const AssemblyLine = props => {
     return allButtons
   }
   
-  const stages = props.stages.map ((stage, index) => {
+  const stages = props.stages.map ((stage, index) => { //mapping des entetes de chaque colonne
     return (
       <div
         data-testid="stage"
@@ -37,29 +37,29 @@ const AssemblyLine = props => {
   })
 
   function addNewButton () {
-    const updatedObject = buttonsColumns
-    updatedObject[props.stages[0]].unshift(newInput)
+    const updatedObject = {...buttonsColumns} //Utiliser la syntaxe avec le spread operator
+    updatedObject[props.stages[0]].unshift(newInput) // ajout en haut de la colonne
     setButtonsColumns (updatedObject)
-    setNewInput("")
+    setNewInput("") //vidage du champ input
   }
 
-  function moveButtonToLeft (stage, input, i) {
-    const updatedObject = {...buttonsColumns}
+  function moveButtonToLeft (stage, input, i) { //déplacement vers la gauche en haut de la colonne ou sortie du tableau
+    const updatedObject = {...buttonsColumns} //Utiliser la syntaxe avec le spread operator
     updatedObject[stage]= updatedObject[stage].filter((x,indexOfx) => (x !== input) || (indexOfx!==i)) // gestion des doublons
     let positionForward = props.stages.indexOf(stage)+1
     if (positionForward<props.stages.length) {
-      updatedObject[props.stages[positionForward]].unshift(input)
+      updatedObject[props.stages[positionForward]].unshift(input) //en haut de la colonne
     }
     setButtonsColumns (updatedObject)
   }
     
-  function moveButtonToRight (e, stage, input, i) {
-    e.preventDefault()
-    const updatedObject = {...buttonsColumns}
+  function moveButtonToRight (e, stage, input, i) { //déplacement vers la droite en bas de la colonne ou sortie du tableau
+    e.preventDefault() //Empecher le comportement par défaut du clic droit du bouton de la souris
+    const updatedObject = {...buttonsColumns} //Utiliser la syntaxe avec le spread operator
     updatedObject[stage]= updatedObject[stage].filter((x,indexOfx) => (x !== input) || (indexOfx!==i)) // gestion des doublons
     let positionBackward = props.stages.indexOf(stage)-1
     if (positionBackward>=0) {
-      updatedObject[props.stages[positionBackward]].push(input)
+      updatedObject[props.stages[positionBackward]].push(input) //en bas de la colonne
     }
     setButtonsColumns (updatedObject)
   }
@@ -70,7 +70,7 @@ const AssemblyLine = props => {
       <input
         name = "newButton"
         data-testid = "add-item"
-        onKeyDown = {e => e.key === "Enter" ? addNewButton() : null}
+        onKeyDown = {e => e.key === "Enter" ? addNewButton() : null} //validation saisie avec la touche Entrée
         onChange = {e => setNewInput(e.target.value)}
         value = {newInput}
       />
